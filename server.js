1856e6972a207e28a86e82d7f2ed2dee85a1fc9f
Apelng <administrator@apelasset.com>
2025-07-24 04:22:00 +0100

    allow origins

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS middleware at the very top
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', allowedOrigins);
+  res.header('Access-Control-Allow-Origin',process.env.LIVE_FRONTEND2);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
